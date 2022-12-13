e530c6612437866d84e53541e088b1786eb02a5c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 16:56:01 +0100

    Entity Account

--- src/main/java/ma/bankconnect/entity/Account.java
+++ src/main/java/ma/bankconnect/entity/Account.java
@@ -1,2 +1,21 @@
-package ma.bankconnect.entity;public class Account {
+package ma.bankconnect.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Entity
+@Getter
+@Setter
+@Table(name = "account")
+public class Account implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
 }
