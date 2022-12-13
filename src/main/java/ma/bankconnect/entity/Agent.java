69266bb9d4803deb11ba522935e3a8ebf7b7e704
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 11:58:02 +0100

    Add poprites agent

--- src/main/java/ma/bankconnect/entity/Agent.java
+++ src/main/java/ma/bankconnect/entity/Agent.java
@@ -1,4 +1,32 @@
 package ma.bankconnect.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+
+@Entity
+@Setter
+@Getter
+
 public class Agent {
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "nom", nullable = false)
+    private String nom;
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
 }
+
