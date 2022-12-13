07043e28cf45643fd9d04b194673e19164f90558
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 16:56:25 +0100

    Entity Customer

--- src/main/java/ma/bankconnect/entity/Customer.java
+++ src/main/java/ma/bankconnect/entity/Customer.java
@@ -1,4 +1,37 @@
 package ma.bankconnect.entity;
-
-public class Customer {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+@Entity
+@Setter
+@Getter
+public class Customer implements Serializable {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+    @Column(name = "nom", nullable = false)
+    private String name;
+    @Column(name = "adresse", nullable = false)
+    private String address;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "telephone", nullable = false)
+    private String phoneNumber;
+    @Column(name = "cin", nullable = false, unique = true)
+    private String cin;
+    public Customer() {
+    }
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", cin='" + cin + '\'' +
+                '}';
+    }
 }
