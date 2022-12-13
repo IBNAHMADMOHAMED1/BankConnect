bb25fe32d56db9c285db1d307b36ba3bf367688e
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 11:26:44 +0100

    first commit

---
+++ src/main/java/ma/bankconnect/BankConnectApplication.java
@@ -0,0 +1,13 @@
+package ma.bankconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/bankconnect/BankConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bankconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
