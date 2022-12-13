365f686c63c4c01b4c52513bc86456b7c0966ce9
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 14:15:55 +0100

    Working in sms configration

--- src/main/java/ma/bankconnect/utils/Sms.java
+++ src/main/java/ma/bankconnect/utils/Sms.java
@@ -1,4 +1,14 @@
 package ma.bankconnect.utils;
 
+import com.twilio.http.TwilioRestClient;
+import com.twilio.type.PhoneNumber;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.message.MessageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
 public class Sms {
+    @Autowired
+    private Environment en;
+    TwilioRestClient client = new TwilioRestClient(en.getProperty("ACCOUNT_SID"), en.getProperty("twilio.auth.token"));
 }
