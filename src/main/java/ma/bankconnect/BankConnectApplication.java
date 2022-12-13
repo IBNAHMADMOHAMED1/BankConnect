c5c32c114497be7867ab346e431cd75416e364be
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 14:59:15 +0100

    Testing

--- src/main/java/ma/bankconnect/BankConnectApplication.java
+++ src/main/java/ma/bankconnect/BankConnectApplication.java
@@ -1,5 +1,6 @@
 package ma.bankconnect;
 
+import ma.bankconnect.utils.Sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class BankConnectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankConnectApplication.class, args);
+        Sms sms = new Sms();
+       // sms.sendSms("+212684632770", "Hello World");
     }
 
 }
