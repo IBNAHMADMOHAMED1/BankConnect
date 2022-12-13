d944d967935f976256ec9874dd7eccfb4f817d57
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 14:59:50 +0100

    modified agent

--- src/main/java/ma/bankconnect/entity/Agent.java
+++ src/main/java/ma/bankconnect/entity/Agent.java
@@ -1,7 +1,6 @@
 package ma.bankconnect.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,22 +10,43 @@ import java.io.Serializable;
 @Entity
 @Setter
 @Getter
+@Table(name = "agent")
+public class Agent implements Serializable {
 
-public class Agent {
-
-    @Getter(AccessLevel.NONE)
-    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
+
     @Column(name = "nom", nullable = false)
     private String nom;
+
     @Column(name = "prenom", nullable = false)
     private String prenom;
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
     @Column(name = "password", nullable = false)
     private String password;
+
+    public Agent(Long id, String nom, String prenom, String email, String password) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
 
