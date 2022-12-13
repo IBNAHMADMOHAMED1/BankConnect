f9fe0b0917fa3d91745236c96321c6b7a2f0345c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-13 15:01:26 +0100

    Set up TWILIO Sms

--- src/main/java/ma/bankconnect/utils/Sms.java
+++ src/main/java/ma/bankconnect/utils/Sms.java
@@ -1,14 +1,21 @@
 package ma.bankconnect.utils;
 
-import com.twilio.http.TwilioRestClient;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import org.apache.logging.log4j.message.Message;
-import org.apache.logging.log4j.message.MessageFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Sms {
-    @Autowired
-    private Environment en;
-    TwilioRestClient client = new TwilioRestClient(en.getProperty("ACCOUNT_SID"), en.getProperty("twilio.auth.token"));
+    @Value("${ACCOUNT_SID}")
+    private String accountSid;
+    @Value("${AUTH_TOKEN}")
+    private String authToken;
+    @Value("${TWILIO_NUMBER}")
+    private String twilioNumber;
+    public Boolean sendSms(String to, String body) {
+        // Set up account SID and auth token
+        Twilio.init(accountSid, authToken);
+        Message message = Message.creator(new PhoneNumber(to), new PhoneNumber(twilioNumber), body).create();
+        return message.getSid() != null;
+    }
 }
