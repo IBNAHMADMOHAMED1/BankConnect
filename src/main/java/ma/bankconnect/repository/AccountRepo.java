f9b6f8e66856bfd130e68ac768199482a07ad772
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-15 16:59:29 +0100

    Account Repo

--- src/main/java/ma/bankconnect/repository/AccountRepo.java
+++ src/main/java/ma/bankconnect/repository/AccountRepo.java
@@ -1,2 +1,14 @@
-package ma.bankconnect.repository;public interface AccountRepo {
+package ma.bankconnect.repository;
+
+import ma.bankconnect.entity.Account;
+import ma.bankconnect.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccountRepo extends JpaRepository<Account, Long> {
+    Account findByAccountNumber(String accountNumber);
+    Account findByCustomer(Customer customer);
+    List<Account> findAllByCustomer(Customer customer);
 }
+
