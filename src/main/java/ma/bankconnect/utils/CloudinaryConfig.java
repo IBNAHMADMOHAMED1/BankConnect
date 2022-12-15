fb4c4f981cc1ffeff50a7f4599bcc3b5dd3a2f8c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-15 15:21:03 +0100

    Configration cloud

--- src/main/java/ma/bankconnect/utils/CloudinaryConfig.java
+++ src/main/java/ma/bankconnect/utils/CloudinaryConfig.java
@@ -1,2 +1,19 @@
-package ma.bankconnect.utils;public class CloudinaryConfig {
+package ma.bankconnect.utils;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "ditimjxfy",
+                "api_key", "146139492622934",
+                "api_secret", "nF94rFrBb-bJV35__ju_YXC_6AQ"
+        ));
+    }
 }
+
