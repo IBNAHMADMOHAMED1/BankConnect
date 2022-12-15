277cf32c4583e52c8fd3148aa57b65d780280097
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-15 16:59:01 +0100

    repository customer

--- src/main/java/ma/bankconnect/repository/CustomerRepo.java
+++ src/main/java/ma/bankconnect/repository/CustomerRepo.java
@@ -1,2 +1,10 @@
-package ma.bankconnect.repository;public interface CustomerRepo {
+package ma.bankconnect.repository;
+
+import ma.bankconnect.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+    Customer findByCin(String cin);
 }
+
--- src/main/java/ma/bankconnect/service/CustomerService.java
+++ src/main/java/ma/bankconnect/service/CustomerService.java
@@ -1,2 +1,10 @@
-package ma.bankconnect.service;public interface CustomerService {
+package ma.bankconnect.service;
+
+import ma.bankconnect.entity.Customer;
+
+public interface CustomerService {
+    Customer save(Customer customer);
+    Customer findByEmail(String email);
+    Customer findByCin(String cin);
+
 }
