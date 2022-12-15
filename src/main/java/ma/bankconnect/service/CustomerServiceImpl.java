aee6c8d8ff33c2c2c405acc81b5d435499ec69f2
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-15 16:59:57 +0100

    Account implontation service

--- src/main/java/ma/bankconnect/service/CustomerServiceImpl.java
+++ src/main/java/ma/bankconnect/service/CustomerServiceImpl.java
@@ -1,2 +1,35 @@
-package ma.bankconnect.service;public class CustomerServiceImpl {
+package ma.bankconnect.service;
+
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.entity.Customer;
+import ma.bankconnect.repository.AccountRepo;
+import ma.bankconnect.repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+    private final AccountRepo accountRepo;
+
+    @Override
+    public Customer save(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    @Override
+    public Customer findByEmail(String email) {
+        return customerRepo.findByEmail(email);
+    }
+
+    @Override
+    public Customer findByCin(String cin) {
+        return customerRepo.findByCin(cin);
+    }
+
+
+
 }
+
