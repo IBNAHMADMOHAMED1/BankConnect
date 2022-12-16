ee36f5bdbed4b51a01de12ab280dfa2456f08d51
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-16 09:16:40 +0100

    add Resource customer

--- src/main/java/ma/bankconnect/api/CustomerResource.java
+++ src/main/java/ma/bankconnect/api/CustomerResource.java
@@ -1,2 +1,20 @@
-package ma.bankconnect.api;public class CustomerResource {
+package ma.bankconnect.api;
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.entity.Customer;
+import ma.bankconnect.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CustomerResource {
+    private final CustomerService customerService;
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return ResponseEntity.ok().body(customerService.getAllCustomers());
+    }
 }
