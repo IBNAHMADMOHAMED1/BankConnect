a4dd620cffd09d73ff1a6954a5799128027428f2
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-16 16:46:00 +0100

    Jwt utils

--- src/main/java/ma/bankconnect/utils/JwtUtils.java
+++ src/main/java/ma/bankconnect/utils/JwtUtils.java
@@ -1,2 +1,55 @@
-package ma.bankconnect.utils;public class JwtUtils {
+package ma.bankconnect.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public class JwtUtils {
+    private String jwtSecret = "secret";
+    private int jwtExpirationMs = 86400000; // 1 day
+    // we need to generate a token and check if the token is valid
+    public Boolean validateJwtToken(String authToken) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken).getBody().getExpiration().after(new Date());
+    }
+    public String getRoleFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+    public String parseJwt(String jwt) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt).getBody().getSubject();
+    }
+
+    // extractEmail
+    public String extractEmail(String token) {
+       return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+    }
+
+// isTokenValid
+    public Boolean isTokenValid(String token , UserDetails userDetails) {
+        final String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername()) && validateJwtToken(token));
+    }
+
+    public String createJwtToken(Map<String, Object> claims, UserDetails userDetails)
+    {
+        return Jwts.builder().setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .claim("authorities", userDetails.getAuthorities())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 }
