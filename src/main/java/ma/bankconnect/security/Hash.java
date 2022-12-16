156baeb271fc0f6f8493933a9ff2b64a2f376768
Redadaalabi2001 <daalabir@gmail.com>
2022-12-16 10:09:22 +0100

    add mail and hash

--- src/main/java/ma/bankconnect/security/Hash.java
+++ src/main/java/ma/bankconnect/security/Hash.java
@@ -1,4 +1,17 @@
 package ma.bankconnect.security;
 
+import de.mkammerer.argon2.*;
+
 public class Hash {
+
+    static Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 32, 64);
+
+    public static String getHashedPassword(String password){
+        return argon2.hash(2,15*1024,1, password.toCharArray());
+    }
+
+    public static Boolean verifiedPassword(String hashedPassword, String password){
+        return argon2.verify(hashedPassword, password.toCharArray());
+    }
+
 }
--- src/main/java/ma/bankconnect/utils/Mail.java
+++ src/main/java/ma/bankconnect/utils/Mail.java
@@ -1,4 +1,69 @@
 package ma.bankconnect.utils;
 
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class Mail {
+
+    public static void sendMail(String userEmail, String msg) {
+        // Sender's email ID needs to be mentioned
+        String email = "daalabireda@gmail.com";
+        String password = "gwlzvspijaxggbqk";
+        //daalabir@gmail.com
+        //udfcuhgoqjdwloan
+        String smtp = "smtp.gmail.com";
+        String port = "25";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host",smtp);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(email, password);
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            assert email != null;
+            message.setFrom(new InternetAddress(email));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
+
+            // Set Subject: header field
+            message.setSubject("Service de Banque centrale du Maroc : Les informations de votre compte");
+
+            // Now set the actual message
+            message.setText(msg);
+
+            System.out.println("envoyer...");
+            // Send message
+            Transport.send(message);
+            System.out.println("message envoyer avec succées....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
 }
