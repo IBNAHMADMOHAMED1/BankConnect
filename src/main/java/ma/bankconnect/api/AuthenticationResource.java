ed6b5dad22af21e30675e9d84d9dfe9603dacfda
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-19 09:10:47 +0100

    Auth api

--- src/main/java/ma/bankconnect/api/AuthenticationResource.java
+++ src/main/java/ma/bankconnect/api/AuthenticationResource.java
@@ -1,2 +1,42 @@
-package ma.bankconnect.api;public class AuthenticationResource {
+package ma.bankconnect.api;
+
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.dto.AuthenticationRequest;
+import ma.bankconnect.utils.JwtUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationResource {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> login(@RequestBody AuthenticationRequest authenticationRequest) {
+
+        System.out.println(authenticationRequest.getEmail());
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        System.out.println("hhh------------------");
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+        if (userDetails != null) {
+            return ResponseEntity.ok().body(jwtUtils.generateToken(userDetails));
+        }
+        return ResponseEntity.badRequest().body("Login failed");
+    }
 }
