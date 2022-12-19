6fb96483891a7deb0ebb36a3f01aaa5a35c144dc
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-19 15:09:08 +0100

    api for test

--- src/main/java/ma/bankconnect/api/CustomerResource.java
+++ src/main/java/ma/bankconnect/api/CustomerResource.java
@@ -1,20 +1,17 @@
 package ma.bankconnect.api;
 
-import lombok.RequiredArgsConstructor;
-import ma.bankconnect.service.CustomerService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("customers")
-@RequiredArgsConstructor
+@RequestMapping("/clients")
 public class CustomerResource {
-    private final CustomerService customerService;
+    //private final CustomerService customerService;
 
-    @GetMapping("all")
-    public ResponseEntity<String> getAllCustomers() {
+    @PostMapping("/clients")
+    public ResponseEntity<String> getAllCustomers(  ) {
         System.out.println("---------1111111111111111------");
         return ResponseEntity.ok("hhhhhhhhhhhhhhh");
     }
