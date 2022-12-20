dde2b96b92fed0e0d68de2d31cfe2e2134044f95
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-20 11:52:02 +0100

    Save customer

--- src/main/java/ma/bankconnect/dto/CustomerRequest.java
+++ src/main/java/ma/bankconnect/dto/CustomerRequest.java
@@ -1,2 +1,25 @@
-package ma.bankconnect.dto;public class CustomerRequest {
+package ma.bankconnect.dto;
+
+import lombok.Data;
+@Data
+public class CustomerRequest {
+    private String email;
+    private String password;
+    private String adresse;
+    private String nom;
+    private String telephone;
+    private String cin;
+    private String cinImage;
 }
+
+/*
+  {
+    "email": "test@test.com"
+    "password": "test"
+    "adresse": "test"
+    "nom": "test"
+    "telephone": "test"
+    "cin": "test"
+    "cinImage": "test"
+    }
+ */
