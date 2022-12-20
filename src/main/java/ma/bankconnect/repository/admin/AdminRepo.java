d6bbda77e43d927eba9091bfa6c1661c0769a108
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-20 11:03:32 +0100

    Admin repo

--- src/main/java/ma/bankconnect/repository/admin/AdminRepo.java
+++ src/main/java/ma/bankconnect/repository/admin/AdminRepo.java
@@ -1,2 +1,10 @@
-package ma.bankconnect.repository.admin;public interface AdminRepo {
+package ma.bankconnect.repository.admin;
+
+import ma.bankconnect.entity.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Agent, Long> {
+    Agent findByEmail(String email);
 }
