f7d9d6e3da0bdcbaaf8907ce1bf6445086170721
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-20 11:01:32 +0100

    add anotation to repo

--- src/main/java/ma/bankconnect/repository/AccountRepo.java
+++ src/main/java/ma/bankconnect/repository/AccountRepo.java
@@ -3,9 +3,10 @@ package ma.bankconnect.repository;
 import ma.bankconnect.entity.Account;
 import ma.bankconnect.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
     Account findByAccountNumber(String accountNumber);
     Account findByCustomer(Customer customer);
