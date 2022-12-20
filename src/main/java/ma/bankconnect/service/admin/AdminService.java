d8adb24a5c50b4335cfab5b9c051a1b139651f20
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-20 11:03:55 +0100

    Admin Service

--- src/main/java/ma/bankconnect/service/admin/AdminService.java
+++ src/main/java/ma/bankconnect/service/admin/AdminService.java
@@ -1,4 +1,8 @@
-package ma.bankconnect.service;
+package ma.bankconnect.service.admin;
+
+import ma.bankconnect.entity.Agent;
 
 public interface AdminService {
+
+    Agent findByEmail(String email);
 }
