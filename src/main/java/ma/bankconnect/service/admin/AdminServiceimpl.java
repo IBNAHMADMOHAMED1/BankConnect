891e08a4b6ef41a0e99859d5118478026594d1e1
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-20 10:59:53 +0100

    Implimontation Admin service

--- src/main/java/ma/bankconnect/service/admin/AdminServiceimpl.java
+++ src/main/java/ma/bankconnect/service/admin/AdminServiceimpl.java
@@ -1,2 +1,15 @@
-package ma.bankconnect.service.admin;public class AdminServiceimpl {
+package ma.bankconnect.service.admin;
+
+import ma.bankconnect.entity.Agent;
+import ma.bankconnect.repository.admin.AdminRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceimpl implements AdminService {
+    private AdminRepo adminRepo;
+
+    @Override
+    public Agent findByEmail(String email) {
+        return adminRepo.findByEmail(email);
+    }
 }
