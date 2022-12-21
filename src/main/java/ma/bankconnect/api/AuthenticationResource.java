43cb3fab4534f537244f9bb895f6fd2c2aefc33b
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 11:31:31 +0100

    create regster

--- src/main/java/ma/bankconnect/api/AuthenticationResource.java
+++ src/main/java/ma/bankconnect/api/AuthenticationResource.java
@@ -1,18 +1,21 @@
 package ma.bankconnect.api;
 
 import lombok.RequiredArgsConstructor;
+import ma.bankconnect.config.CloudinaryConfig;
 import ma.bankconnect.dao.UserDao;
 import ma.bankconnect.dto.AuthenticationRequest;
 import ma.bankconnect.dto.CustomerRequest;
 import ma.bankconnect.entity.Customer;
 import ma.bankconnect.service.CustomerServiceImpl;
 import ma.bankconnect.utils.JwtUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -23,6 +26,7 @@ public class AuthenticationResource {
     private final UserDetailsService userDetailsService;
     private final UserDao userDao;
     private final CustomerServiceImpl customerService;
+    private final CloudinaryConfig cloud;
 
     @PostMapping("/authenticate")
     public ResponseEntity<String> login(@RequestBody AuthenticationRequest authenticationRequest) {
@@ -35,12 +39,16 @@ public class AuthenticationResource {
         );
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
         if (userDetails != null) {
+            System.out.println("userDetails = " + userDetails);
             return ResponseEntity.ok().body(jwtUtils.generateToken(userDetails));
         }
         return ResponseEntity.badRequest().body("Bad credentials");
     }
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody CustomerRequest customerRequest) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<String> register(@ModelAttribute CustomerRequest customerRequest) {
+        if (customerService.findByEmail(customerRequest.getEmail()) != null) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
         Customer customer = new Customer();
         customer.setName(customerRequest.getNom());
         customer.setAddress(customerRequest.getAdresse());
@@ -48,8 +56,9 @@ public class AuthenticationResource {
         customer.setPassword(customerRequest.getPassword());
         customer.setPhoneNumber(customerRequest.getTelephone());
         customer.setCin(customerRequest.getCin());
-        customer.setCinImage(customerRequest.getCinImage());
-
+        MultipartFile file = customerRequest.getFile();
+        String url = cloud.uploadImage(file);
+        customer.setCinImage(url);
         Customer newCustomer =  customerService.save(customer);
         if (newCustomer != null) {
             return ResponseEntity.ok().body("Customer created");
