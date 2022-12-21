efdef98431d49dcba3cf7e0ae4cda0a6e4f7d4e6
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 15:56:41 +0100

    Test autherization

--- src/main/java/ma/bankconnect/api/CustomerResource.java
+++ src/main/java/ma/bankconnect/api/CustomerResource.java
@@ -1,16 +1,21 @@
 package ma.bankconnect.api;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/clients")
+@RequestMapping("api/v1/customers")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class CustomerResource {
     //private final CustomerService customerService;
-
-    @PostMapping("all")
+    @GetMapping("all")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_CUSTOMER')")
     public ResponseEntity<String> getAllCustomers(  ) {
         System.out.println("---------1111111111111111------");
         return ResponseEntity.ok("hhhhhhhhhhhhhhh");
