09df0c86efef2f04b104f155cebb63e565b13bb1
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 15:58:12 +0100

    Add class CustomAuthenticationEntryPoint

--- src/main/java/ma/bankconnect/ControllerAdvice/CustomAuthenticationEntryPoint.java
+++ src/main/java/ma/bankconnect/ControllerAdvice/CustomAuthenticationEntryPoint.java
@@ -1,2 +1,26 @@
-package ma.bankconnect.ControllerAdvice;public class CustomAuthenticationEntryPoint {
+package ma.bankconnect.ControllerAdvice;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint  implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // Set the response status to 401 (Unauthorized)
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        // Set the response content type to JSON
+        response.setContentType("application/json");
+
+        // Write the error message to the response body
+        response.getWriter().println("{\"error\": Invalid credentials}");
+
+    }
 }
