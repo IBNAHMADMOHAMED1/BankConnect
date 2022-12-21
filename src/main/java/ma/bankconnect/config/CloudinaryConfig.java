62b5b6b9cdc6c8f696bfc7ed2947fe473a008cde
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 11:33:00 +0100

    Config Cloud and uplaod image

--- src/main/java/ma/bankconnect/config/CloudinaryConfig.java
+++ src/main/java/ma/bankconnect/config/CloudinaryConfig.java
@@ -1,18 +1,31 @@
-package ma.bankconnect.utils;
+    package ma.bankconnect.config;
 
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import org.springframework.context.annotation.Configuration;
+    import com.cloudinary.Cloudinary;
+    import com.cloudinary.utils.ObjectUtils;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.web.multipart.MultipartFile;
 
-@Configuration
-public class CloudinaryConfig {
+    import java.io.IOException;
+    import java.util.Map;
 
-    public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "ditimjxfy",
-                "api_key", "146139492622934",
-                "api_secret", "nF94rFrBb-bJV35__ju_YXC_6AQ"
-        ));
+    @Configuration
+    public class CloudinaryConfig {
+
+        private final String CLOUDINARY_URL="cloudinary://146139492622934:nF94rFrBb-bJV35__ju_YXC_6AQ@bankconnect";
+        public String uploadImage(MultipartFile image)  {
+            Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+                    "cloud_name", "bankconnect",
+                    "api_key", "146139492622934",
+                    "api_secret", "nF94rFrBb-bJV35__ju_YXC_6AQ"));
+
+            Map uploadResult ;
+            try {
+                uploadResult = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.emptyMap());
+                return uploadResult.get("url").toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
-}
 
