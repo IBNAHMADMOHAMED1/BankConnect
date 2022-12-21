32de2be4e488365368aef1f69624782dfc3193e1
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 15:57:22 +0100

    Add Methode WhoWanntToLogin

--- src/main/java/ma/bankconnect/config/JwtAuthFilter.java
+++ src/main/java/ma/bankconnect/config/JwtAuthFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.Setter;
 import ma.bankconnect.utils.JwtUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,10 +19,13 @@ import java.io.IOException;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @Component
+@Getter
+@Setter
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtils jwtUtils;
     private final UserDetailsService userDetailsService;
+    private String token;
 
     public JwtAuthFilter(JwtUtils jwtUtils, UserDetailsService userDetailsService) {
         this.jwtUtils = jwtUtils;
@@ -40,12 +45,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authHeader.substring(7);
+        setToken(jwtToken);
         userEmail = jwtUtils.extractUsername(jwtToken);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
             //final Boolean isTokenValid;
             if (jwtUtils.isTokenValid(jwtToken, userDetails)) {
-                System.out.println("token valid");
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
@@ -54,5 +59,21 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
+    }
+    public String[] extractAuthorities(String token) {
+        return jwtUtils.extractAuthorities(token);
+    }
+    public String getWhoWantToLogin() {
+        String[] authorities = extractAuthorities(getToken());
+        System.out.println("token: " + getToken());
+        System.out.println("authorities: " + authorities[0]);
+        String whoWantToLogin = authorities[0].substring(12, authorities[0].length() - 2);
+        if (whoWantToLogin.equals("ROLE_CUSTOMER")) {
+            return "customer";
+        }
+        if (whoWantToLogin.equals("ROLE_ADMIN")) {
+            return "admin";
+        }
+        return null;
     }
 }
