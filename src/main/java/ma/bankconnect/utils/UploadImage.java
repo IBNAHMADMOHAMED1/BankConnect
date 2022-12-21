56ab8320fa3a8880fee463db5c21630ff522897f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-21 11:30:34 +0100

    Config Cloud and uplaod image

--- src/main/java/ma/bankconnect/utils/UploadImage.java
+++ src/main/java/ma/bankconnect/utils/UploadImage.java
@@ -1,2 +1,13 @@
-package ma.bankconnect.utils;public class UploadImage {
+package ma.bankconnect.utils;
+
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.config.CloudinaryConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UploadImage {
+        private final CloudinaryConfig cloudinaryConfig;
+
+
 }
