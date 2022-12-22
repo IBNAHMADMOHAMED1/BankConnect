f8c6c55e37cf74109688a8cc82ab5762f8d5cbcf
Redadaalabi2001 <daalabir@gmail.com>
2022-12-22 10:10:11 +0100

    solve account and customer conflict

--- src/main/java/ma/bankconnect/entity/Agent.java
+++ src/main/java/ma/bankconnect/entity/Agent.java
@@ -1,21 +1,16 @@
 package ma.bankconnect.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
+import lombok.Data;
 import java.io.Serializable;
 
+@Data
 @Entity
-@Setter
-@Getter
-@RequiredArgsConstructor
 @Table(name = "agent")
 public class Agent implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
@@ -31,24 +26,6 @@ public class Agent implements Serializable {
     @Column(name = "password", nullable = false)
     private String password;
 
-    public Agent(Long id, String nom, String prenom, String email, String password) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Agent{" +
-                "id=" + id +
-                ", nom='" + nom + '\'' +
-                ", prenom='" + prenom + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
 
 
