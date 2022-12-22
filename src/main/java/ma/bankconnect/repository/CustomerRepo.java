320d7b2c18262a8d86e27e2ddda3fcda539962f3
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-22 09:37:21 +0100

    Customer Repository

--- src/main/java/ma/bankconnect/repository/CustomerRepo.java
+++ src/main/java/ma/bankconnect/repository/CustomerRepo.java
@@ -2,9 +2,12 @@ package ma.bankconnect.repository;
 
 import ma.bankconnect.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
     Customer findByEmail(String email);
     Customer findByCin(String cin);
+
 }
 
