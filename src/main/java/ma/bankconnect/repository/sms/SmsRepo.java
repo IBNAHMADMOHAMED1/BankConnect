c0095e62b5b0e8e015964a02f73452396edfa21c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-22 09:39:30 +0100

    Sms Repo

--- src/main/java/ma/bankconnect/repository/sms/SmsRepo.java
+++ src/main/java/ma/bankconnect/repository/sms/SmsRepo.java
@@ -1,2 +1,11 @@
-package ma.bankconnect.repository.sms;public interface SmsRepo {
+package ma.bankconnect.repository.sms;
+
+import ma.bankconnect.entity.Sms;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SmsRepo extends JpaRepository<Sms,Long> {
+    Sms findByCode(String code);
+    Sms findByCustomerId(Long customerId);
 }
