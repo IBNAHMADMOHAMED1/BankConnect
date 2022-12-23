887eadc974e35ad0d5c86870c80a8ca780903813
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:37 +0100

    Service: add new service for operation

--- src/main/java/ma/bankconnect/service/operation/OperationService.java
+++ src/main/java/ma/bankconnect/service/operation/OperationService.java
@@ -1,2 +1,9 @@
-package ma.bankconnect.service.operation;public interface OperationService {
+package ma.bankconnect.service.operation;
+
+import ma.bankconnect.entity.AccountOperation;
+
+public interface OperationService {
+    Boolean transfer(AccountOperation accountOperation);
+    Boolean deposit(AccountOperation accountOperation);
+    Boolean withdraw(AccountOperation accountOperation);
 }
