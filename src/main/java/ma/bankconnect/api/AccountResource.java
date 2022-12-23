2c879285a03f1059e052046618a4821b832172ce
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 09:31:25 +0100

    Account Resource

--- src/main/java/ma/bankconnect/api/AccountResource.java
+++ src/main/java/ma/bankconnect/api/AccountResource.java
@@ -1,2 +1,29 @@
-package ma.bankconnect.api;public class AccountResource {
+package ma.bankconnect.api;
+
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.entity.Account;
+import ma.bankconnect.service.account.AccountServiceImpl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/account")
+public class AccountResource {
+
+    private final AccountServiceImpl accountService;
+
+    @PostMapping("create")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_CUSTOMER')")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        System.out.println("cousomer id: " + account.getCustomer().getId());
+        Account newAccount = accountService.createAccount(account);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(newAccount);
+    }
+
 }
