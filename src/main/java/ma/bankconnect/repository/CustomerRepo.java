85420c9a912e77d6e7d217d3306e402490f5c079
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 09:34:48 +0100

    Customer repository

--- src/main/java/ma/bankconnect/repository/CustomerRepo.java
+++ src/main/java/ma/bankconnect/repository/CustomerRepo.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
     Customer findByEmail(String email);
     Customer findByCin(String cin);
+    Long findIdByEmail(String email);
 
 
 }
