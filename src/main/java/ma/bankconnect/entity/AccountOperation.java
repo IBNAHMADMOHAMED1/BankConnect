022e7b04e8f379e06f92ec68c8998f9790e0814d
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    Entity: add new entity for account operation

--- src/main/java/ma/bankconnect/entity/AccountOperation.java
+++ src/main/java/ma/bankconnect/entity/AccountOperation.java
@@ -1,2 +1,29 @@
-package ma.bankconnect.entity;public class AccountOperation {
+package ma.bankconnect.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder // Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountOperation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate operationDate;
+    private double amount;
+    @Column(name = "operation_type",columnDefinition = "varchar(255) default 'DEPOSIT'")
+    private String type;
+    @ManyToOne
+    private Account accountTo;
+    @ManyToOne
+    private Account accountFrom;
+
 }
