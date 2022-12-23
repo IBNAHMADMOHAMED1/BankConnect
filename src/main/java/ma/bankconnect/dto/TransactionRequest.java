9cac3d783827d47f9640851e89f6f50c17d52d6b
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    DTO: add new DTO for transaction

--- src/main/java/ma/bankconnect/dto/TransactionRequest.java
+++ src/main/java/ma/bankconnect/dto/TransactionRequest.java
@@ -1,2 +1,13 @@
-package ma.bankconnect.dto;public class TransactionRequest {
+package ma.bankconnect.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TransactionRequest {
+    private String AccountNumberFrom;
+    private Double amount;
+    private String type;
+    private String AccountNumberTo;
 }
