933dec743f145d7e1b491c7be7949145e79099cd
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    Repository: add new repository for operation

--- src/main/java/ma/bankconnect/repository/operation/OperationRepo.java
+++ src/main/java/ma/bankconnect/repository/operation/OperationRepo.java
@@ -1,2 +1,9 @@
-package ma.bankconnect.repository.operation;public interface OperationRepo {
+package ma.bankconnect.repository.operation;
+
+import ma.bankconnect.entity.AccountOperation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepo extends JpaRepository<AccountOperation, Long> {
 }
