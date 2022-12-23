e1d07a8fd34dcb97a5659de49ed445b48b141916
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    Repository: add new repository for transaction

--- src/main/java/ma/bankconnect/repository/transaction/TransactionRepo.java
+++ src/main/java/ma/bankconnect/repository/transaction/TransactionRepo.java
@@ -1,2 +1,12 @@
-package ma.bankconnect.repository.transaction;public interface TransactionRepo {
+package ma.bankconnect.repository.transaction;
+
+import ma.bankconnect.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepo extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByAccountId(Long accountId);
 }
