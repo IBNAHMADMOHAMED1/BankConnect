b000774b25c25c46d6ed5eef979c7d50243aaca9
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:37 +0100

    Service: add new service for transaction

--- src/main/java/ma/bankconnect/service/transaction/TransactionService.java
+++ src/main/java/ma/bankconnect/service/transaction/TransactionService.java
@@ -1,2 +1,13 @@
-package ma.bankconnect.service.transaction;public interface TransactionService {
+package ma.bankconnect.service.transaction;
+
+import ma.bankconnect.entity.Transaction;
+
+import java.util.List;
+
+public interface TransactionService {
+    Transaction save(Transaction transaction);
+    Transaction findByCode(String code);
+    Transaction findByCustomerId(Long customerId);
+    Transaction findByAccountId(Long accountId);
+    List<Transaction> getAllTransactions(Long customerId);
 }
