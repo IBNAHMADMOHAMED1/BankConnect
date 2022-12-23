6df33b5fc4b3e5110a313b1cfe6ab397abfefc9e
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    Entity: add new entity for transfer

--- src/main/java/ma/bankconnect/entity/Transfer.java
+++ src/main/java/ma/bankconnect/entity/Transfer.java
@@ -1,2 +1,46 @@
-package ma.bankconnect.entity;public class Transfer {
+package ma.bankconnect.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Transfer {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Basic
+        @Column(name = "rec_account", nullable = false)
+        private Long recAccount; //receiver account
+
+        @Basic
+        @Column(name = "transaction_id", nullable = false)
+        private Long transactionId; //transaction id
+
+        @Basic
+        @Column(name = "status", nullable = false)
+        private boolean status; //status of the transfer
+
+        @ManyToOne
+        @JoinColumn(name = "rec_account", insertable = false, updatable = false)
+        private Account account;
+
+        @ManyToOne
+        @JoinColumn(name = "transaction_id", insertable = false, updatable = false)
+        private Transaction transaction;
+        @Basic @Column
+        private Date created;
+        @Basic @Column
+        private Date updated;
+        @PrePersist
+        protected void onCreate(){
+            this.created = new Date();
+            this.updated = new Date();
+        }
+        @PreUpdate
+        protected void onUpdate(){
+            this.updated = new Date();
+        }
+
 }
