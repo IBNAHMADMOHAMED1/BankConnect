1b45846135daccceb1787b2b26784e613d3edb3e
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:39 +0100

    Service: add new service implementation for transaction

--- src/main/java/ma/bankconnect/service/transaction/TransactionServiceImpl.java
+++ src/main/java/ma/bankconnect/service/transaction/TransactionServiceImpl.java
@@ -1,2 +1,42 @@
-package ma.bankconnect.service.transaction;public class TransactionServiceImpl {
+package ma.bankconnect.service.transaction;
+
+import lombok.RequiredArgsConstructor;
+import ma.bankconnect.entity.Transaction;
+import ma.bankconnect.repository.transaction.TransactionRepo;
+import ma.bankconnect.service.account.AccountServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionServiceImpl implements TransactionService {
+    private final TransactionRepo transactionRepo;
+    private final AccountServiceImpl accountService;
+
+    @Override
+    public Transaction save(Transaction transaction) {
+        return null;
+    }
+
+    @Override
+    public Transaction findByCode(String code) {
+        return null;
+    }
+
+    @Override
+    public Transaction findByCustomerId(Long customerId) {
+        return null;
+    }
+
+    @Override
+    public Transaction findByAccountId(Long accountId) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions(Long customerId) {
+        Long accountId = accountService.findAccountByCustomerId(customerId).getId();
+        return transactionRepo.findAllByAccountId(accountId);
+    }
 }
