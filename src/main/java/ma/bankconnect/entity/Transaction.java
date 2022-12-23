efc761275f7766a40694b29d7090454d110c8dd3
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-12-23 17:22:36 +0100

    Entity: add new entity for transaction

--- src/main/java/ma/bankconnect/entity/Transaction.java
+++ src/main/java/ma/bankconnect/entity/Transaction.java
@@ -2,29 +2,45 @@ package ma.bankconnect.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import java.io.Serializable;
+
+import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "transaction")
-public class Transaction implements Serializable {
+public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Basic
     @Column(name = "type", nullable = false)
     private String type;
 
-    @Column(name = "montant", nullable = false)
-    private Double montant;
+    @Basic
+    @Column(name = "amount", nullable = false)
+    private Double amount;
 
+    @Basic
     @Column(name = "account_id", nullable = false)
     private Long accountId;
 
     @ManyToOne
     @JoinColumn(name = "account_id", insertable = false, updatable = false)
     private Account account;
-
+    @Basic
+    @Column
+    private Date created;
+    @Basic
+    @Column
+    private Date updated;
+    @PrePersist
+    protected void onCreate() {
+        this.created = new Date();
+        this.updated = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated = new Date();
+    }
 }
